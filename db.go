32589fb20a88a697a390e1281b4f3c64d7a65ee2
Giovanni Mazzeo <mazzeo.gio@gmail.com>
2023-11-04 00:01:02 +0100

    Update db.go

--- db.go
+++ db.go
@@ -433,8 +433,8 @@ func (db *DB) mmap(minsz int) error {
 	}
 
 	// Ensure the size is at least the minimum size.
-	db.filesz = int(info.Size())
-	var size = db.filesz
+	db.fileSize() = int(info.Size())
+	var size = db.fileSize()
 
 	if size < minsz {
 		size = minsz
@@ -446,7 +446,7 @@ func (db *DB) mmap(minsz int) error {
 
 	if db.Mlock {
 		// Unlock db memory
-		if err := db.munlock(db.filesz); err != nil {
+		if err := db.munlock(db.fileSize()); err != nil {
 			return err
 		}
 	}
@@ -470,7 +470,7 @@ func (db *DB) mmap(minsz int) error {
 
 	if db.Mlock {
 		// Don't allow swapping of data file
-		if err := db.mlock(db.filesz); err != nil {
+		if err := db.mlock(db.fileSize()); err != nil {
 			return err
 		}
 	}
